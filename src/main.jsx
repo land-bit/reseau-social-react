6966f55135511edbd4f55d11ee82cfe4a8f3d899
landrybit <landrybitege7@gmail.com>
2024-03-03 21:36:28 +0200

    Page inscription

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Dashboards from "./pages/dashboards/Dashboards.jsx";
 import Inscription from "./pages/inscription/Inscription.jsx";
